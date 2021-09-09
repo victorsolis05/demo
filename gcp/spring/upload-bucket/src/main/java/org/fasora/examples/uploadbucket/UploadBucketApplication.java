0f5856560ccc169dcd83bd726fd654fb1f3d648f
Victor Hugo Solis Ramos <vhsr_pepe@hotmail.com>
2021-09-09 13:19:00 -0500

    upload project

---
+++ gcp/spring/upload-bucket/src/main/java/org/fasora/examples/uploadbucket/UploadBucketApplication.java
@@ -0,0 +1,13 @@
+package org.fasora.examples.uploadbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadBucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadBucketApplication.class, args);
+	}
+
+}
---
+++ gcp/spring/upload-bucket/src/test/java/org/fasora/examples/uploadbucket/UploadBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasora.examples.uploadbucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadBucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
